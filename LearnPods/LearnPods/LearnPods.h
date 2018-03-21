6e09deb9186e8eec71541beccd6c72008f430d85
niyulong <niuyulong1991@gmail.com>
2018-03-21 13:32:10 +0800

    初始化项目，项目类型为Framework

---
+++ LearnPods/LearnPods/LearnPods.h
@@ -0,0 +1,19 @@
+//
+//  LearnPods.h
+//  LearnPods
+//
+//  Created by niyulong on 2018/3/21.
+//  Copyright © 2018年 niyulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LearnPods.
+FOUNDATION_EXPORT double LearnPodsVersionNumber;
+
+//! Project version string for LearnPods.
+FOUNDATION_EXPORT const unsigned char LearnPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LearnPods/PublicHeader.h>
+
+
